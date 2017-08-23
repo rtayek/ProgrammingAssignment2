ffcead61091b859f8be4350d652ce37142118411
Ray Tayek <ray@tayek.com>
2017-08-22 21:28:52 -0700

    first

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## make a special vector to cach the inverse of a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      inverse <- NULL
+      set <- function(y) {
+            inverse <<- y
+            inverse <<- NULL
+      }
+      get <- function()
+            x
+      setinverse <- function(inverse_)
+            inverse <<- inverse_
+      getinverse <- function()
+            inverse
+      list(
+            set = set,
+            get = get,
+            setinverse = setinverse,
+            getinverse = getinverse
+      )
 }
 
 
-## Write a short comment describing this function
+## invert a matrix or us the cached value.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      inverse <- x$getinverse()
+      if (!is.null(inverse)) {
+            message("getting cached inverse")
+            return(inverse)
+      }
+      data <- x$get()
+      inverse <- solve(data)
+      x$setinverse(inverse)
+      inverse
+}
+
+
+## sanity test case
+
+sanityTestCase <- function() {
+      matrix <- diag(3)
+      cacheFunction <- makeCacheMatrix(matrix)
+      inverse = cacheSolve(cacheFunction)
+      inverse2 <- cacheSolve(cacheFunction)
+      product <- matrix %*% inverse
+      if (!all(product == diag(3)))
+            return(FALSE)
+      product2 <- matrix %*% inverse
+      if (!all(product2 == diag(3)))
+            return(FALSE)
+      TRUE
+
 }
